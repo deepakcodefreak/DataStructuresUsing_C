947a6944a66c3352755a4360be77d4781633041e
deepakcodefreak <deepak19.engg@gmail.com>
2018-10-15 12:14:50 +0530

    find height of BSt

---
+++ binarySearchTree/heightofBST.cpp
@@ -0,0 +1,105 @@
+
+#include<iostream>
+
+using namespace std;
+
+
+class node{
+public:
+    int data;
+    node *left;
+    node *right;
+};
+
+node* createNode(int data){
+    node *newnode = new node();
+    newnode->data = data;
+    return newnode;
+}
+
+node* insert(node *root,int data)
+{
+
+    if(root==NULL)
+    {
+        root = createNode(data);
+    }else if(data<=root->data){
+        root->left = insert(root->left,data);
+    }else{
+        root->right = insert(root->right,data);
+    }
+        return root;
+}
+
+int findMax(node *root){
+    if(root==NULL)
+    {
+        cout<<"Tree is empty\n";
+        return -1;
+    }
+
+    node *counter = root;
+
+    if(counter->right==NULL)
+    {
+        return counter->data;
+    }
+
+    return findMax(counter->right);
+
+}
+
+int findMin(node *root){
+    if(root==NULL)
+    {
+        cout<<"Tree is empty\n";
+        return -1;
+    }
+
+    node *counter = root;
+    if(counter->left == NULL)
+    {
+        return counter->data;
+
+    }
+
+    return findMin(counter->left);
+
+
+}
+
+int max(int x,int y)
+{
+    if(x>y)
+      return x;
+    else
+      return y;
+}
+
+
+int findHeight(node *root)
+{
+    if(root == NULL)
+     return -1;
+
+    return max(findHeight(root->left),findHeight(root->right))+1;
+}
+
+
+int main()
+{
+    node *root = NULL;
+    root = insert(root,10);
+    root = insert(root,5);
+    root = insert(root,15);
+    root = insert(root,2);
+    root = insert(root,25);
+    root = insert(root,20);
+    cout<<findHeight(root);
+
+
+    //cout<<findMin(root)<<endl;
+    //cout<<findMax(root);
+    return 0;
+}
+
